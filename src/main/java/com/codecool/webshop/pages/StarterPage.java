dc812918f36fed1eff1b93850a8be3b8c4caa743
TiborKovari <142129834+TiborKovari@users.noreply.github.com>
2024-08-19 22:17:37 +0200

    starter page added to pages

---
+++ src/main/java/com/codecool/webshop/pages/StarterPage.java
@@ -0,0 +1,41 @@
+package com.codecool.webshop.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class StarterPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    @FindBy(id="user-name")
+    private WebElement userNameInput;
+
+    @FindBy(id="password")
+    private WebElement passwordInput;
+
+    @FindBy(id="login-button")
+    private WebElement loginButton;
+
+    public StarterPage(WebDriver driver){
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    public void fillUserNameInput(String userName){
+        wait.until(ExpectedConditions.visibilityOf(userNameInput)).sendKeys(userName);
+    }
+
+    public void fillPasswordInput(String password){
+        wait.until(ExpectedConditions.visibilityOf(passwordInput)).sendKeys(password);
+    }
+
+    public void clickLoginButton(){
+        wait.until(ExpectedConditions.visibilityOf(loginButton)).click();
+    }
+}
