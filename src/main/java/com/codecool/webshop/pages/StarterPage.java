8fa681a11563851edb9f220644dacb8e56b9d796
andrastoth021 <andrastoth021@gmail.com>
2024-08-21 16:31:37 +0200

    add cart feature's step definition class

--- src/main/java/com/codecool/webshop/pages/StarterPage.java
+++ src/main/java/com/codecool/webshop/pages/StarterPage.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class StarterPage {
-    private WebDriver driver;
+    protected WebDriver driver;
     protected WebDriverWait wait;
     @FindBy(id="user-name")
     public WebElement userNameInput;
