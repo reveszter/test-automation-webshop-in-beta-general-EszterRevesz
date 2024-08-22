74ddd94e8cdd0cfa1613cc270c57d2306d0d124b
TiborKovari <142129834+TiborKovari@users.noreply.github.com>
2024-08-22 10:34:28 +0200

    pom with feature and steps are created well

---
+++ src/main/java/com/codecool/webshop/pages/ProductPage.java
@@ -0,0 +1,46 @@
+package com.codecool.webshop.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPage{
+    private WebDriver driver;
+    @FindBy(id = "react-burger-menu-btn")
+    private WebElement burgerMenuButton;
+
+    @FindBy(id="back-to-products")
+    private WebElement backToProductsButton;
+
+    @FindBy(id="add-to-cart")
+    private WebElement addToCartButton;
+
+    @FindBy(css = "div.inventory_details_name.large_size")
+    private WebElement productName;
+
+    @FindBy(css = "div.inventory_details_desc.large_size")
+    private WebElement productDescription;
+
+    @FindBy(css = "div.inventory_details_price")
+    private WebElement productPrice;
+
+    public ProductPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getProductName() {
+        return productName.getText();
+    }
+
+    public String getProductDescription() {
+        return productDescription.getText();
+    }
+
+    public String getProductPrice() {
+        return productPrice.getText();
+    }
+
+
+}
---
+++ src/test/java/com/codecool/webshop/stepdefinitions/ShowProductSteps.java
@@ -0,0 +1,35 @@
+package com.codecool.webshop.stepdefinitions;
+
+import com.codecool.webshop.pages.HomeLoggedInPage;
+import com.codecool.webshop.pages.ProductPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+
+public class ShowProductSteps extends Utils{
+
+    private HomeLoggedInPage homeLoggedInPage;
+    private ProductPage productPage;
+
+    @Given("User is logged in on the homepage")
+    public void user_is_logged_in_on_the_homepage(){
+        openNewDriver();
+        loginUser();
+    }
+
+    @When("User select an item by clicking")
+    public void user_select_an_item_by_clicking(){
+        homeLoggedInPage = new HomeLoggedInPage(webDriver);
+        homeLoggedInPage.clickOnFirstProductInTheList();
+    }
+
+    @Then("Details of the selected product can be seen on the screen")
+    public void details_of_the_selected_product_can_be_seen_on_the_screen(){
+        productPage = new ProductPage(webDriver);
+        String productDescription = productPage.getProductDescription();
+        Assertions.assertNotNull(productDescription, "Product description should not be null");
+        Assertions.assertFalse(productDescription.isEmpty(), "Product description should not be empty");
+        webDriver.quit();
+    }
+}
